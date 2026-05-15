ef4e16b809e617699029b5ae197a73e318bf8256
Maheswari Dian P <maheswaridian@icloud.com>
2026-05-15 14:24:44 +0700

    riwayat

--- controllers/riwayat_controller.py
+++ controllers/riwayat_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, redirect
+from flask import Blueprint, render_template, session, redirect, request
 from utils.db import get_db_connection
 
 riwayat_bp = Blueprint('riwayat', __name__)
@@ -10,23 +10,49 @@ def riwayat():
     if 'user' not in session:
         return redirect('/')
 
+    # ambil keyword pencarian
+    search = request.args.get('search', '')
+
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
 
-    # ambil data log activity
-    cursor.execute("""
-            SELECT 
-                l.id_log,
-                l.id_pengajuan,
-                l.id_user,
-                u.nama,
-                l.jenis_aktivitas,
-                l.aktivitas,
-                l.waktu
-            FROM log_activity l
-            JOIN users u ON l.id_user = u.id_user
-            ORDER BY l.waktu DESC
-        """)
+    query = """
+        SELECT 
+            l.id_log,
+            l.id_pengajuan,
+            l.id_user,
+            u.nama,
+            l.jenis_aktivitas,
+            l.aktivitas,
+            l.waktu
+        FROM log_activity l
+        JOIN users u ON l.id_user = u.id_user
+    """
+
+    params = []
+
+    # jika ada pencarian
+    if search:
+        query += """
+            WHERE 
+                l.id_pengajuan LIKE %s
+                OR u.nama LIKE %s
+                OR l.jenis_aktivitas LIKE %s
+                OR l.aktivitas LIKE %s
+        """
+
+        keyword = f"%{search}%"
+
+        params.extend([
+            keyword,
+            keyword,
+            keyword,
+            keyword
+        ])
+
+    query += " ORDER BY l.waktu DESC"
+
+    cursor.execute(query, params)
 
     logs = cursor.fetchall()
 
@@ -35,5 +61,6 @@ def riwayat():
 
     return render_template(
         'riwayat.html',
-        logs=logs
+        logs=logs,
+        search=search
     )
